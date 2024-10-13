4d87fc4842e64f6266ca74c7a4dfbf5462f503cb
ryumaeda <ryumaeda@gmail.com>
2024-10-13 15:15:58 +0900

    initial commit

--- App.tsx
+++ App.tsx
@@ -7,25 +7,26 @@ import {
   SafeAreaView,
   StyleSheet,
   TextInput,
+  TouchableOpacity,
 } from "react-native";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 
 const App = () => {
   const [animation] = useState(new Animated.ValueXY({ x: 145, y: 360 }));
-  const [moved, setMoved] = useState(false);
+  const [flagThink, setflagThink] = useState(false);
+  const [flagAdd, setflagAdd] = useState(false);
   const [isMoving, setIsMoving] = useState(false);
   const [members, setMembers] = useState([]);
   const [newMemberName, setNewMemberName] = useState("");
   const [newMemberGender, setNewMemberGender] = useState("");
-  const [showAddMember, setShowAddMember] = useState(false);
 
-  const moveIcon = () => {
+  const moveToThinking = () => {
     Animated.timing(animation, {
       toValue: { x: 30, y: 700 },
       duration: 350,
       useNativeDriver: true,
     }).start(() => {
-      setMoved(true);
+      setflagThink(true);
       setIsMoving(false);
     });
   };
@@ -37,25 +38,54 @@ const App = () => {
       duration: 350,
       useNativeDriver: true,
     }).start(() => {
-      setMoved(false);
+      setflagThink(false);
+      setflagAdd(false);
       setIsMoving(false);
     });
   };
 
   const addMember = () => {
-    setShowAddMember(true);
+    setIsMoving(true);
+    Animated.timing(animation, {
+      toValue: { x: 30, y: 700 },
+      duration: 350,
+      useNativeDriver: true,
+    }).start(() => {
+      setflagAdd(true);
+      setIsMoving(false);
+    });
   };
 
-  const saveMember = () => {
+  const setMember = () => {
     setMembers([...members, { name: newMemberName, gender: newMemberGender }]);
-    setShowAddMember(false);
     setNewMemberName("");
     setNewMemberGender("");
+    setflagAdd(false);
+    Animated.timing(animation, {
+      toValue: { x: 145, y: 360 },
+      duration: 350,
+      useNativeDriver: true,
+    }).start(() => {
+      setflagThink(false);
+      setIsMoving(false);
+    });
+  };
+
+  const closeAddMember = () => {
+    setflagAdd(false);
+    Animated.timing(animation, {
+      toValue: { x: 145, y: 360 },
+      duration: 350,
+      useNativeDriver: true,
+    }).start(() => {
+      setflagThink(false);
+      setIsMoving(false);
+    });
   };
 
   return (
     <SafeAreaView className="flex-1">
-      {/* アニメーションのアイコンは絶対座標で表示 */}
+      {/* Icon (絶対座標で表示)*/}
       <Animated.View
         style={[
           styles.iconContainer,
@@ -65,8 +95,8 @@ const App = () => {
         <Icon name="emoticon-happy" size={100} color="#4CAF50" />
       </Animated.View>
 
-      {/* 初期画面の上部にメンバーリストを表示 */}
-      {!moved && (
+      {/* 初期画面*/}
+      {!flagThink && !flagAdd && (
         <View className="ml-10 mt-10 flex flex-wrap flex-row">
           {members.map((member, index) => (
             <View
@@ -89,13 +119,21 @@ const App = () => {
           ))}
         </View>
       )}
+      <View className="flex-1 justify-center items-center mt-40">
+        {!flagThink && !isMoving ? (
+          <>
+            <Button title="add member" onPress={addMember} />
+            <Button title="think game" onPress={moveToThinking} />
+          </>
+        ) : null}
+      </View>
 
-      {/* think gameボタン押下後 */}
-      <View className="">
-        {moved && !isMoving ? (
-          <View>
+      {/* think画面(ゲームを考案させる) */}
+      {flagThink && !isMoving ? (
+        <View>
+          <View className="mb-96">
             <View>
-              <Text className="ml-10 mt-20 text-xl font-bold">＃メンバー</Text>
+              <Text className="text-xl ml-10 font-bold">＃メンバー</Text>
               <Text className="text-lg ml-10">
                 {members.map((member, index) => (
                   <Text key={index}>
@@ -113,47 +151,62 @@ const App = () => {
                 placeholder="とにかく盛り上がるやつ"
               />
             </View>
-          </View>
-        ) : null}
-      </View>
-
-      {/* 初期画面のボタン */}
-      <View className="flex-1 justify-center items-center mt-40">
-        {!moved && !isMoving ? (
-          <>
-            <Button title="add member" onPress={addMember} />
-            <Button title="think game" onPress={moveIcon} />
-          </>
-        ) : null}
-      </View>
-      <View className="">
-        {moved && !isMoving ? (
-          <View>
-            <View className="mb-10">
+            <View className="mt-72">
               <Button title="戻る" onPress={resetIcon} />
             </View>
           </View>
-        ) : null}
-      </View>
-
-      {/* メンバー追加画面 */}
-      {showAddMember && (
-        <View className="absolute top-0 left-0 right-0 bottom-0 bg-white justify-center items-center">
-          <TextInput
-            className="h-10 border border-gray-400 mx-4 my-2 p-10 bg-white shadow-lg"
-            placeholder="名前"
-            value={newMemberName}
-            onChangeText={setNewMemberName}
-          />
-          <TextInput
-            className="h-10 border border-gray-400 mx-4 my-2 p-10 bg-white shadow-lg"
-            placeholder="性別"
-            value={newMemberGender}
-            onChangeText={setNewMemberGender}
-          />
-          <Button title="保存" onPress={saveMember} />
         </View>
-      )}
+      ) : null}
+
+      {/* add画面(メンバーを追加する) */}
+      {flagAdd && !isMoving ? (
+        <View>
+          <View className="mb-96">
+            <View>
+              <TextInput
+                className="h-10 border border-gray-400 mx-4 my-2 p-10 bg-white shadow-lg"
+                placeholder="名前"
+                value={newMemberName}
+                onChangeText={setNewMemberName}
+              />
+            </View>
+            <View className="flex-row mx-4 my-2">
+              <TouchableOpacity
+                className={`flex-1 h-10 justify-center items-center ${
+                  newMemberGender === "男" ? "bg-blue-500" : "bg-gray-200"
+                }`}
+                onPress={() => setNewMemberGender("男")}
+              >
+                <Text
+                  className={`${
+                    newMemberGender === "男" ? "text-white" : "text-black"
+                  }`}
+                >
+                  男
+                </Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                className={`flex-1 h-10 justify-center items-center ${
+                  newMemberGender === "女" ? "bg-pink-500" : "bg-gray-200"
+                }`}
+                onPress={() => setNewMemberGender("女")}
+              >
+                <Text
+                  className={`${
+                    newMemberGender === "女" ? "text-white" : "text-black"
+                  }`}
+                >
+                  女
+                </Text>
+              </TouchableOpacity>
+            </View>
+            <Button title="保存" onPress={setMember} />
+            <View className="mt-80">
+              <Button title="戻る" onPress={resetIcon} />
+            </View>
+          </View>
+        </View>
+      ) : null}
     </SafeAreaView>
   );
 };
